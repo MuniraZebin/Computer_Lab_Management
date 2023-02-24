e2897b742fc0de9fbf46e8c71f867bb7bfb36a6f
Munira Zebin <86850398+MuniraZebin@users.noreply.github.com>
2023-02-25 04:25:02 +0600

    displayPC and searchPC features done

--- lab.py
+++ lab.py
@@ -7,8 +7,22 @@ class Lab:
         self.os = ""
         self.status = ""
 
-    def display_pc(self):
-        pass
+    def display_pcs(self):
+        with open("./Lab.json") as file_obj:
+            pcs = json.load(file_obj)
+
+        if len(pcs) > 0:
+            for i in pcs:
+                print("\nPC number:", i['pc_id'])
+                print("Operating System:", i['pc_os'])
+                print("Current status:", i['pc_status'])
+
+            choice = input("\nWant to search any PC to display?(y/n): ")
+            if choice == 'y' or choice == 'Y':
+                self.search_pc()
+
+        else:
+            print("No PC registered yet")
 
     def add_pc(self):
         valid = True
@@ -43,15 +57,36 @@ class Lab:
         with open(f"./Lab.json", "w") as file:
             json.dump(pcs, file)
 
+        print("PC has been added!")
+
     def update_pc(self):
         valid = False
         with open("./Lab.json") as file_obj:
             pcs = json.load(file_obj)
         try:
             while True:
-                self.pc_number = int(input("Enter PC number you want to update information: "))
+                if self.pc_number == 0:
+                    self.pc_number = int(input("Enter PC number you want to update information: "))
+
                 for i in pcs:
                     if i['pc_id'] == self.pc_number:
+                        choice = 0
+                        while 1 > choice or choice > 2:
+                            choice = int(input("Which info you want to update?\n"
+                                               "1. Operating System. \n"
+                                               "2. PC current status.\n\n"
+                                               "Enter: "))
+                        if choice == 1:
+                            self.os = input("Enter Operating System: ")
+                            i['pc_os'] = self.os
+                            with open("./Lab.json", "w") as file_update:
+                                json.dump(pcs, file_update)
+
+                        elif choice == 2:
+                            self.status = input("Enter current status: ")
+                            i['pc_status'] = self.status
+                            with open("./Lab.json", "w") as file_update:
+                                json.dump(pcs, file_update)
                         valid = True
                         break
 
@@ -60,30 +95,11 @@ class Lab:
                 else:
                     print("PC does not exist!\n")
                     valid = False
-
-            choice = 0
-            while 1 > choice or choice > 2:
-                choice = int(input("Which info you want to update?\n"
-                                   "1. Operating System. \n"
-                                   "2. PC current status.\n\n"
-                                   "Enter: "))
-
-            for i in pcs:
-                if i['pc_id'] == self.pc_number:
-                    if choice == 1:
-                        self.os = input("Enter Operating System: ")
-                        i['pc_os'] = self.os
-                        with open("./Lab.json", "w") as file_update:
-                            json.dump(pcs, file_update)
-
-                    elif choice == 2:
-                        self.status = input("Enter current status: ")
-                        i['pc_status'] = self.status
-                        with open("./Lab.json", "w") as file_update:
-                            json.dump(pcs, file_update)
         except:
             print("Invalid Input!\n")
             self.update_pc()
+
+        print("Information updated!")
 
     def remove_pc(self):
         valid = False
@@ -91,9 +107,10 @@ class Lab:
             pcs = json.load(file_obj)
 
         while True:
-            self.pc_number = int(input("Enter PC number you want to update information: "))
-            for i in pcs:
+            self.pc_number = int(input("Enter PC number you want to remove: "))
+            for num, i in enumerate(pcs):
                 if i['pc_id'] == self.pc_number:
+                    pcs.pop(num)
                     valid = True
                     break
 
@@ -103,16 +120,32 @@ class Lab:
                 print("PC does not exist!\n")
                 valid = False
 
-        with open("./Lab.json") as file:
-            data = json.load(file)
-
-        for num, i in enumerate(data):
-            if i['pc_id'] == self.pc_number:
-                data.pop(num)
-                break
-
         with open("./Lab.json", "w") as file_update:
-            json.dump(data, file_update)
+            json.dump(pcs, file_update)
+
+        print("PC has been removed!")
 
     def search_pc(self):
-        pass
+        valid = False
+        with open("./Lab.json") as file_obj:
+            pcs = json.load(file_obj)
+
+        while True:
+            self.pc_number = int(input("Enter PC number you want to search: "))
+            for i in pcs:
+                if i['pc_id'] == self.pc_number:
+                    print("\nPC number:", i['pc_id'])
+                    print("Operating System:", i['pc_os'])
+                    print("Current status:", i['pc_status'])
+                    choice = input("\nWant to update this PC information?(y/n): ")
+                    if choice == 'y' or choice == 'Y':
+                        self.update_pc()
+
+                    valid = True
+                    break
+
+            if valid:
+                break
+            else:
+                print("PC does not exist!\n")
+                valid = False
--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from lab import Lab
 
 
 def main():
-    pc = Lab()
-    print("Name: \nId: \nComputer Lab Management Application\n")
+    print("Name: Munira Zebin \nId: 19-41014-2 \nComputer Lab Management Application\n")
     while True:
+        pc = Lab()
         print("\n1. Add a new PC")
         print("2. Update information of an existing PC")
         print("3. Remove an existing PC")
-        print("4. Display information about all the PCs")
-        print("5. Display all information of a PC")
-        print("6. Search for a particular PC and display the information")
-        print("7. Store all the PC available in the application into a text file")
-        print("8. Quit")
+        print("4. Display all the PCs")
+        print("5. Search for a particular PC")
+        print("0. Quit")
         user_choice = input("\nEnter your choice: ")
         if user_choice == '1':
             pc.add_pc()
@@ -24,7 +22,13 @@ def main():
         elif user_choice == '3':
             pc.remove_pc()
 
-        elif user_choice == '8':
+        elif user_choice == '4':
+            pc.display_pcs()
+
+        elif user_choice == '5':
+            pc.search_pc()
+
+        elif user_choice == '0':
             print("Application Terminated!")
             break
 
